8677092bb919e2bf9d07311ab1ebaa013ec0b51f
Makzzzimus <2009katita@gmail.com>
2024-03-24 16:07:58 +0200

    opening v2, bug fixes

--- script.js
+++ script.js
@@ -121,6 +121,7 @@ function flagCell(t){
                 }
                 if(correctFlaggedQuantity==10){
                     statusBar.innerHTML = "You Won!";
+                    isGameOver = true;
                     clearInterval(stopwatch);
                     for (let i=500; i<3500; i=i+1000){
                         setTimeout(()=>{statusBar.style.color = "green";}, i);
@@ -151,44 +152,182 @@ function generateMines(){
 }
 
 function opening(t){
+    let emptyCellsArray = [];
     let tempCell = t;
+    
+    console.log(tempCell.id);
     //let onLeftBoundary = false;
     //let onRightBoundary = false;
-    //let onUpperBoundary = false;
-    //let onLowerBoundary = false;
+    // let onUpperBoundary = true;
+    // let onLowerBoundary = true;
     let upperCell = Number(tempCell.id) + 10, lowerCell = Number(tempCell.id) -10, rightCell = Number(tempCell.id) + 1, leftCell = Number(tempCell.id) -1, upperRightCell = Number(tempCell.id) - 9, upperLeftCell = Number(tempCell.id) - 11, lowerRightCell = Number(tempCell.id) + 11, lowerLeftCell = Number(tempCell.id) + 9;
 
-    if (document.getElementById(upperCell).innerHTML === "⠀"){
-        tempCell = document.getElementById(upperCell);
-        checkSurroundingCells(tempCell);
-    }
-    if (document.getElementById(lowerCell).innerHTML === "⠀"){
-        tempCell = document.getElementById(lowerCell);
-        checkSurroundingCells(tempCell);
-    }
-    if (document.getElementById(leftCell).innerHTML === "⠀"){
-        tempCell = document.getElementById(leftCell);
-        checkSurroundingCells(tempCell);
-    }
-    if (document.getElementById(rightCell).innerHTML === "⠀"){
-        tempCell = document.getElementById(rightCell);
-        checkSurroundingCells(tempCell);
-    }
-    if (document.getElementById(upperLeftCell).innerHTML === "⠀"){
-        tempCell = document.getElementById(upperLeftCell);
-        checkSurroundingCells(tempCell);
-    }
-    if (document.getElementById(upperRightCell).innerHTML === "⠀"){
-        tempCell = document.getElementById(upperRightCell);
-        checkSurroundingCells(tempCell);
-    }
-    if (document.getElementById(lowerLeftCell).innerHTML === "⠀"){
-        tempCell = document.getElementById(lowerLeftCell);
-        checkSurroundingCells(tempCell);
-    }
-    if (document.getElementById(lowerRightCell).innerHTML === "⠀"){
-        tempCell = document.getElementById(lowerRightCell);
-        checkSurroundingCells(tempCell);
+    if (tempCell !== null && tempCell !== undefined){
+        
+    //       if ((Number(tempCell.id[1]) - 10) > 0){ //Is not on Upper Boundary
+    //         if (document.getElementById(upperCell).innerHTML === "⠀" && document.getElementById(upperCell).style.background !== "rgb(225, 225, 255)"){
+    //             tempCell = document.getElementById(upperCell);
+    //             if (checkSurroundingCells(tempCell) === true){
+    //                 emptyCellsArray.push(tempCell);
+    //             }
+    //         }
+    //         onUpperBoundary = false;
+    //     }
+    //     if ((Number(tempCell.id[1]) + 10) < 101){ //Is not on Lower Boundary
+    //         if (document.getElementById(lowerCell).innerHTML === "⠀" && document.getElementById(lowerCell).style.background !== "rgb(225, 225, 255)"){
+    //             tempCell = document.getElementById(lowerCell);
+    //             if (checkSurroundingCells(tempCell) === true){
+    //                 emptyCellsArray.push(tempCell);
+    //             }
+    //         }
+    //         onLowerBoundary = false;
+    //     }
+
+    //     if (tempCell.id[1] !== "0"){ //Is not on Right Boundary
+    //         if (document.getElementById(rightCell).innerHTML === "⠀" && document.getElementById(rightCell).style.background !== "rgb(225, 225, 255)"){
+    //             tempCell = document.getElementById(rightCell);
+    //             if (checkSurroundingCells(tempCell) === true){
+    //                 emptyCellsArray.push(tempCell);
+    //             }
+    //             if (onUpperBoundary == false){
+    //                 if (document.getElementById(upperRightCell).innerHTML === "⠀" && document.getElementById(upperRightCell).style.background !== "rgb(225, 225, 255)"){
+    //                     tempCell = document.getElementById(upperRightCell);
+    //                     if (checkSurroundingCells(tempCell) === true){
+    //                         emptyCellsArray.push(tempCell);
+    //                     }
+    //                 }
+    //             }
+    //             if (onLowerBoundary == false){
+    //                 if (document.getElementById(lowerRightCell).innerHTML === "⠀" && document.getElementById(lowerRightCell).style.background !== "rgb(225, 225, 255)"){
+    //                     tempCell = document.getElementById(lowerRightCell);
+    //                     if (checkSurroundingCells(tempCell) === true){
+    //                         emptyCellsArray.push(tempCell);
+    //                     }
+    //                 }
+    //             }
+    //         }
+
+    //     }
+    //     if (tempCell.id[1] !== "1"){ //Is not on Left Boundary
+    //         if (document.getElementById(leftCell).innerHTML === "⠀" && document.getElementById(leftCell).style.background !== "rgb(225, 225, 255)"){
+    //             tempCell = document.getElementById(leftCell);
+    //             if (checkSurroundingCells(tempCell) === true){
+    //                 emptyCellsArray.push(tempCell);
+    //             }
+    //             if (onUpperBoundary == false){
+    //                 if (document.getElementById(upperLeftCell).innerHTML === "⠀" && document.getElementById(upperLeftCell).style.background !== "rgb(225, 225, 255)"){
+    //                     tempCell = document.getElementById(upperLeftCell);
+    //                     if (checkSurroundingCells(tempCell) === true){
+    //                         emptyCellsArray.push(tempCell);
+    //                     }
+    //                 }
+    //             }
+    //             if (onLowerBoundary == false){
+    //                 if (document.getElementById(lowerLeftCell).innerHTML === "⠀" && document.getElementById(lowerLeftCell).style.background !== "rgb(225, 225, 255)"){
+    //                     tempCell = document.getElementById(lowerLeftCell);
+    //                     if (checkSurroundingCells(tempCell) === true){
+    //                         emptyCellsArray.push(tempCell);
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     else if(tempCell.id[1]=== undefined){ //Is not on Left Boundary(exception for first raw)
+    //         if(tempCell.id[0]==="1"){
+    //             if (document.getElementById(leftCell).innerHTML === "⠀" && document.getElementById(leftCell).style.background !== "rgb(225, 225, 255)"){
+    //                 tempCell = document.getElementById(leftCell);
+    //                 if (checkSurroundingCells(tempCell) === true){
+    //                     emptyCellsArray.push(tempCell);
+    //                 }
+    //             }
+    //             if (onUpperBoundary == false){
+    //                 if (document.getElementById(upperLeftCell).innerHTML === "⠀" && document.getElementById(upperLeftCell).style.background !== "rgb(225, 225, 255)"){
+    //                     tempCell = document.getElementById(upperLeftCell);
+    //                     if (checkSurroundingCells(tempCell) === true){
+    //                         emptyCellsArray.push(tempCell);
+    //                     }
+    //                 }
+    //             }
+    //             if (onLowerBoundary == false){
+    //                 if (document.getElementById(lowerLeftCell).innerHTML === "⠀" && document.getElementById(lowerLeftCell).style.background !== "rgb(225, 225, 255)"){
+    //                     tempCell = document.getElementById(lowerLeftCell);
+    //                     if (checkSurroundingCells(tempCell) === true){
+    //                         emptyCellsArray.push(tempCell);
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //           Doesn't work for some reason, and im too lazy to get it working
+        
+        try{
+            if (document.getElementById(upperCell).style.background !== "rgb(225, 225, 255)"){
+                tempCell = document.getElementById(upperCell);
+                if (checkSurroundingCells(tempCell) === true){
+                    emptyCellsArray.push(tempCell);
+                }
+            }
+        }catch{}
+        try{
+            if (document.getElementById(lowerCell).style.background !== "rgb(225, 225, 255)"){
+                tempCell = document.getElementById(lowerCell);
+                if (checkSurroundingCells(tempCell) === true){
+                    emptyCellsArray.push(tempCell);
+                }
+            }
+        }catch{}
+        try{
+            if (document.getElementById(leftCell).style.background !== "rgb(225, 225, 255)"){
+                tempCell = document.getElementById(leftCell);
+                if (checkSurroundingCells(tempCell) === true){
+                    emptyCellsArray.push(tempCell);
+                }
+            }
+        }catch{}
+        try{
+            if (document.getElementById(rightCell).style.background !== "rgb(225, 225, 255)"){
+                tempCell = document.getElementById(rightCell);
+                if (checkSurroundingCells(tempCell) === true){
+                    emptyCellsArray.push(tempCell);
+                }
+            }
+        }catch{}
+        try{
+            if (document.getElementById(lowerRightCell).style.background !== "rgb(225, 225, 255)"){
+                tempCell = document.getElementById(lowerRightCell);
+                if (checkSurroundingCells(tempCell) === true){
+                    emptyCellsArray.push(tempCell);
+                }
+            }
+        }catch{}
+        try{
+            if (document.getElementById(upperLeftCell).style.background !== "rgb(225, 225, 255)"){
+                tempCell = document.getElementById(upperLeftCell);
+                if (checkSurroundingCells(tempCell) === true){
+                    emptyCellsArray.push(tempCell);
+                }
+            }
+        }catch{}
+        try{
+            if (document.getElementById(upperRightCell).style.background !== "rgb(225, 225, 255)"){
+                tempCell = document.getElementById(upperRightCell);
+                if (checkSurroundingCells(tempCell) === true){
+                    emptyCellsArray.push(tempCell);
+                }
+            }
+        }catch{}
+        try{
+            if (document.getElementById(lowerLeftCell).style.background !== "rgb(225, 225, 255)"){
+                tempCell = document.getElementById(lowerLeftCell);
+                if (checkSurroundingCells(tempCell) === true){
+                    emptyCellsArray.push(tempCell);
+                }
+            }
+        }catch{}
+        
+        for (let i=0; emptyCellsArray.length; i++){
+            opening(emptyCellsArray[i]);
+        }
     }
 }
 
@@ -225,8 +364,12 @@ function checkSurroundingCells(tempCell){
     }
     tempCell.style.boxShadow = "inset 1.5px 1.5px 3px 1px rgba(0,0,0,0.55)";
     tempCell.style.background = "rgb(225, 225, 255)";
-    if (nearbyMinesCount !== 0){
+    if (nearbyMinesCount !== 0 && tempCell.innerHTML !== null){
         tempCell.innerHTML = nearbyMinesCount;
     }
+    if (nearbyMinesCount === 0){
+        return true;
+    }
 }
+
 generateMines();
