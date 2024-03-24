25fec36cd6455ff1f16419304513e31c45eb7a74
Makzzzimus <2009katita@gmail.com>
2024-03-24 15:59:07 +0200

    mine generation

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+let minesLocation = [];
+
 function openCell(t){
+    let nearbyMinesCount;
     t.style.boxShadow = "inset 1.5px 1.5px 3px 1px rgba(0,0,0,0.55)"
+    for (let i=0; i<10; i++){
+        if (t.id == minesLocation[i]){
+            alert("boom");
+            t.innerHTML = "💣";
+        }
+    }
+    if (t.innerHTML !== "💣"){
+        if ((t.id % 10) == 0){
+
+        }
+    }
+    //alert(t.id)
 }
+
+function generateMines(){
+    let temp;
+    for (let i=0; i<10; i++){
+        temp = Math.round((Math.random() * 100)+1)
+        for (let i=0; i<10; i++){
+            if (temp == minesLocation[i]){
+                location.reload();
+            }
+        }
+        minesLocation.push(temp);
+    }
+    //alert(minesLocation);
+}
+
+generateMines();
